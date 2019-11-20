622bac528d4a11ea4aeef72c4bda1fa6979cb078
tam <tamulamail@gmail.com>
2019-11-20 21:32:15 +0900

    add fizzbuzz code, then 関数化

---
+++ FizzBuzz2.py
@@ -0,0 +1,22 @@
+def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+
+    else:
+        print(str(number))
+
+
+fizzbuzz_convert(1)
+fizzbuzz_convert(2)
+fizzbuzz_convert(3)
+fizzbuzz_convert(4)
+fizzbuzz_convert(5)
+fizzbuzz_convert(6)
+fizzbuzz_convert(7)
+fizzbuzz_convert(8)
+fizzbuzz_convert(9)
+fizzbuzz_convert(10)
